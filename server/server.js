09efcd8cf48e66d2307b5a6eaeceade623d154a5
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2019-01-13 17:04:01 +0000

    Add newMessahe and createMessage events

--- server/server.js
+++ server/server.js
@@ -13,6 +13,16 @@ app.use(express.static('public'));
 io.on('connection', socket => {
   console.log('New Connection');
 
+  socket.emit('newMessage', {
+    from: 'Fouad Tabbara',
+    text: 'Hey there...',
+    createdAt: new Date()
+  });
+
+  socket.on('createMessage', newMessage => {
+    console.log('createMessage', newMessage);
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected from the browser');
   });
