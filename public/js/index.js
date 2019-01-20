746b189db1fcfbd1ff6a416f901fea8e9b8971fd
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2019-01-20 17:40:49 +0000

    Add form for messages and show incoming messages in ordered listed

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,33 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('New message', message);
+
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+socket.emit(
+  'createMessage',
+  {
+    from: 'Frank',
+    text: 'Hi'
+  },
+  function(serverMessage) {
+    console.log('Got it...', serverMessage);
+  }
+);
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit(
+    'createMessage',
+    {
+      from: 'User',
+      text: $('[name=message]').val()
+    },
+    function() {}
+  );
 });
