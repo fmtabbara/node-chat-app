7ac21d50fadd6122b9b9ea2f641735c149083a74
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2019-01-21 15:02:06 +0000

    added css styling

--- public/js/index.js
+++ public/js/index.js
@@ -31,15 +31,18 @@ socket.on('newLocationMessage', function(message) {
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  const messageTextbox = $('[name=message]');
+
   socket.emit(
     'createMessage',
     {
       from: 'User',
-      text: $('[name=message]').val()
+      text: messageTextbox.val()
     },
-    function() {}
+    function() {
+      messageTextbox.val('');
+    }
   );
-  $('[name=message]').val('');
 });
 
 const locationButton = $('#send-location');
@@ -49,14 +52,18 @@ locationButton.on('click', function() {
     return alert('location is unavailable in this broawser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(
     function(position) {
+      locationButton.removeAttr('disabled').text('Send location');
       socket.emit('createLocationMessage', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     },
     function() {
+      locationButton.removeAttr('disabled').text('Send location');
       alert('Unable to fetch location');
     }
   );
